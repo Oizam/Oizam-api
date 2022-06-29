29e60a096eee0328836b19366a55b820d0982fbc
gsoulat <gsoulat31@gmail.com>
2022-06-29 09:06:18 +0100

    update

--- main.py
+++ main.py
@@ -40,13 +40,13 @@ app.include_router(login.router)
 # app.include_router(predict.router)
 
 
-# lg.info('Database destoy')
-# models.Base.metadata.drop_all(engine)
-# lg.info('Database created!')
-# models.Base.metadata.create_all(engine)
+lg.info('Database destoy')
+models.Base.metadata.drop_all(engine)
+lg.info('Database created!')
+models.Base.metadata.create_all(engine)
 
 lg.info('Database import bird')
-data = pd.read_csv ('.\App\data\OiseauxFini.csv')   
+data = pd.read_csv ('App\data\OiseauxFini.csv')   
 df = pd.DataFrame(data)
 df.to_sql('birds', con = engine, if_exists='append', index=False)
 
